5b56b8f14ea77695e74eb3fb7734b956d1c12cd6
Виктор Озолин <viktor-ozolin@mail.ru>
2021-09-02 17:57:44 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Sidebar from './Sidebar/Sidebar'
+import Header from './Header/Header'
+import Content from './Content/Content'
+import './App.css'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+      <Header />
+      <Content />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Sidebar/SidebarHeader.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from './SidebarHeader.module.css';
+
+
+
+const SidebarHeader = () => {
+
+    return (
+        <React.Fragment>
+            
+        </React.Fragment>
+    );
+}
+
+export default SidebarHeader
